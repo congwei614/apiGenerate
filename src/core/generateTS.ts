a8859064b070cc38827643557b54d27ac610c493
Mr.cong <2534069177@qq.com>
2023-05-06 21:04:39 +0800

    feat(generateTs): :sparkles: 添加格式化Components中的键

--- src/core/generateTS.ts
+++ src/core/generateTS.ts
@@ -1,26 +1,27 @@
 /*
  * @Author: Mr.Cong Wei
  * @Date: 2023-05-05 19:49:13
- * @LastEditTime: 2023-05-05 21:13:48
+ * @LastEditTime: 2023-05-06 21:03:31
  */
 import { isString } from '../share/is';
-import type { Components, Schemas } from '../types/openAPI';
+import type { Components } from '../types/openAPI';
 export default class GenerateTs {
     private components: Components;
+    public result: any = {};
     constructor(components: Components) {
         this.components = components;
         this.init(this.components);
     }
     private init(components: Components) {
         if (!isString(components.schemas)) {
-            // TODO
-            // ts类型不匹配，要实现去除不合法键名
-            // components.schemas = this.filterSchemasKey(components.schemas);
+            components.schemas = this.filterSchemasKey(components);
+            this.formatSchemas(components);
         }
-        console.log(components.schemas);
     }
-    public filterSchemasKey(schemas: Schemas) {
-        if (isString(schemas)) return schemas;
+
+    // 去除不合法键
+    public filterSchemasKey({ schemas }: Components) {
+        if (isString(schemas) || '$ref' in schemas) return schemas;
         const tempSchemas = {};
         const schemasRegex = new RegExp(/^[a-zA-Z0-9.-_]+$/);
         for (const key in schemas) {
@@ -29,5 +30,24 @@ export default class GenerateTs {
             }
         }
         return tempSchemas;
+    }
+
+    // 格式化的键
+    public formatSchemas({ schemas }: Components) {
+        if (isString(schemas) || '$ref' in schemas) return undefined;
+        for (const key in schemas) {
+            const formatKey = this.splitAndUppercase(key);
+            // TODO
+            // 生成将对应的数据生成type对象并写入文件
+            this.result[formatKey] = undefined;
+        }
+        console.log(this.result);
+    }
+
+    // 将键使用._-分割，并将将分割内容首字母大写，最后拼接一起
+    private splitAndUppercase(str: string): string {
+        const regex = /[._-]/;
+        const words = str.split(regex);
+        return words.map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join('');
     }
 }
