0130f54d5d151d06637cefed3a5060a6bd640b93
Mr.cong <2534069177@qq.com>
2023-07-15 12:19:32 +0800

    chore: :hammer: 增加version控制

---
+++ .versionrc.cjs
@@ -0,0 +1,18 @@
+// .versionrc
+
+module.exports = 
+{
+    "types": [
+      { "type": "feat", "section": "需求","hidden": false },
+      { "type": "fix", "section": "Bug 修复" },
+      { "type": "perf", "section": "优化" },
+      { "type": "docs", "hidden": true },
+      { "type": "style", "hidden": true },
+      { "type": "refactor", "hidden": true} ,
+      { "type": "test", "hidden": true },
+      { "type": "build", "hidden": true },
+      { "type": "ci", "hidden": true },
+      { "type": "chore", "hidden": true },
+      { "type": "revert", "hidden": true }
+    ]
+  }
